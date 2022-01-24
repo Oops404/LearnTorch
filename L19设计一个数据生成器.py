36d2c25171f89493415e929d8da6a06f8adc2806
Oops404 <cheneyjin@outlook.com>
2022-01-24 14:34:16 +0800

    rt

---
+++ L19设计一个数据生成器.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8-*-
+"""
+@Project: LearnTorch
+@Author: Oops404
+@Email: cheneyjin@outlook.com
+@Time: 2022/1/24 14:18
+"""
+import random
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import numpy as np
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, TensorDataset, DataLoader
+
+"""
+炼丹师(ง •_•)ง的成长之路，目的是为了控制变量。
+"""
