19c496cb96e2a2f24735dc0b176f88b36f469c63
Oops404 <cheneyjin@outlook.com>
2022-01-25 16:25:48 +0800

    rt

--- L19设计一个数据生成器.py
+++ L19设计一个数据生成器.py
@@ -101,10 +101,85 @@ def data_reg_generator(num_examples=1000, w=None, bias=True, delta=0.01, deg=1):
     return features, labels
 
 
-torch.manual_seed(996)
-_features, _labels = data_reg_generator(deg=2, delta=0.15)
-plt.subplot(121)
-plt.scatter(_features[:, 0], _labels)
-plt.subplot(122)
-plt.scatter(_features[:, 1], _labels)
-plt.show()
+# torch.manual_seed(996)
+# _features, _labels = data_reg_generator(deg=2, delta=0.15)
+# plt.subplot(121)
+# plt.scatter(_features[:, 0], _labels)
+# plt.subplot(122)
+# plt.scatter(_features[:, 1], _labels)
+# plt.show()
+
+# -------------------------------------------------------------------------
+# 多分类数据生成器
+#
+# 随机生成符合，4均值，2标准差的10行2列数据
+# torch.normal(4, 2, size=(10, 2))
+
+# torch.manual_seed(996)
+# _num_inputs = 2
+# _num_examples = 500
+#
+# # 自变量簇
+# data0 = torch.normal(4, 2, size=(_num_examples, _num_inputs))
+# data1 = torch.normal(-2, 2, size=(_num_examples, _num_inputs))
+# data2 = torch.normal(-6, 2, size=(_num_examples, _num_inputs))
+#
+# # 创建012标签
+# label0 = torch.zeros(500)
+# label1 = torch.ones(500)
+# label2 = torch.full_like(label1, 2)
+#
+# _features = torch.cat((data0, data1, data2)).float()
+# _labels = torch.cat((label0, label1, label2)).long().reshape(-1, 1)
+#
+# plt.scatter(_features[:, 0], _features[:, 1], c=_labels)
+# plt.show()
+
+def data_class_generator(num_examples=1000, num_inputs=2, num_class=3, deg_dispersion=None, bias=False):
+    """
+    分类数据集生成器
+    :param num_examples: 每隔类别的数据量
+    :param num_inputs: 数据集特征数量
+    :param num_class: 分类标签总数
+    :param deg_dispersion: 数据分布离散程度参数
+    :param bias: 建立模型逻辑回归模型时是否带入截距
+    :return: 生成特征张量和标签张量，其中特征张量为二维浮点数组
+    """
+    if deg_dispersion is None:
+        deg_dispersion = [4, 2]
+
+    # 每一类特征张量的均值参考值
+    _mean = deg_dispersion[0]
+    # 每一类特征张量的方差
+    _std = deg_dispersion[1]
+    # 用于存储每一类特征张量的列表容器
+    lf = []
+    # 用于存储每一类特征张量的列表容器
+    ll = []
+    # 每一类特征张量的惩罚因子
+    k = _mean * (num_class - 1) / 2
+
+    for i in range(num_class):
+        # 生成每一类的张量
+        data_temp = torch.normal(
+            i * _mean - k,  # 尽可能围绕0分布
+            _std,
+            size=(num_examples, num_inputs)
+        )
+        lf.append(data_temp)
+        # 生成每一类的标签，torch.empty(num_examples, 1)每一类标签张量的形状
+        labels_temp = torch.full_like(torch.empty(num_examples, 1), i)
+        ll.append(labels_temp)
+
+    _features = torch.cat(lf).float()
+    _labels = torch.cat(ll).long()
+
+    if bias:
+        _features = torch.cat((_features, torch.ones(len(_features), 1)), 1)
+    return _features, _labels
+
+
+# _f, _l = data_class_generator()
+#
+# plt.scatter(_f[:, 0], _f[:, 1], c=_l)
+# plt.show()
